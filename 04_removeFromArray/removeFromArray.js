fe951693d5017f76a8ca9d4cb227544fe1ffcd8e
mpotisk1 <mpotisk2@gmail.com>
2025-01-26 16:05:11 -0600

    first iteration of this. saving in case i need to go back

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, remove) {
+
+let i = 0
+let x = 0
+
+let = removeArray = Array.from(remove);
+for(let x in removeArray){
+
+for (let i in list){
+    if(removeArray[x] === list[i]){
+        list.splice(i, 1);
+        i++
+    }
+    else{i++};
+}
+x++;
+}
+
+
+console.log(list);
+console.log(remove);
+console.log(removeArray);
+return list
+
 };
 
+removeFromArray([1, 2, 3, 4], 3, 2);
+
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
