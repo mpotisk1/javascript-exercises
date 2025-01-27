6124087db3391df59a7b3ffd0b15c16e310fcfb2
mpotisk1 <mpotisk2@gmail.com>
2025-01-27 16:33:51 -0600

    I keep forgetting to add these practices to git. Anyway, heres a temp conversion function"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  //subtract 32, multiply by 5/9ths
+  let cToF = 5 / 9
+  let temp = (tempF - 32) * cToF
+  const roundTemp = Math.round(temp*10) / 10
+
+  return roundTemp
+
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(tempC) {
+  //multiply by 9/5ths, add 32
+let cToF = 9 / 5
+let temp = tempC * cToF + 32
+const roundTemp = Math.round(temp*10) / 10
 
+return roundTemp
+
+};
+convertToCelsius(100)
+convertToFahrenheit(100)
 // Do not edit below this line
+
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
